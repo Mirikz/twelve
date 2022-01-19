b7f95698632fd0b9dc8380ceb6370a2b99f60b63
Mirikz <kirillmitrofanov68@gmail.com>
2022-01-19 08:48:06 +0500

    twelve

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = int.Parse(Console.ReadLine());
